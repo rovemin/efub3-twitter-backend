0cb19abb36ded09684f89b32fc93027517416f48
Minseo Cho <min1130@ewhain.net>
2023-06-02 02:22:57 +0900

    feat: WebConfig 추가

---
+++ src/main/java/efub/toy/twitter/global/config/WebConfig.java
@@ -0,0 +1,25 @@
+package efub.toy.twitter.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("")
+                .allowedOrigins("")
+                .allowedMethods(HttpMethod.GET.name())
+                .allowedMethods(HttpMethod.POST.name())
+                .allowedMethods(HttpMethod.PUT.name())
+                .allowedMethods(HttpMethod.DELETE.name())
+                .allowedHeaders("Authorization")
+                .allowedHeaders("refresh-token")
+                .allowCredentials(true);
+    }
+}
