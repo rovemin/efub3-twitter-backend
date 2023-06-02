fc7df59f46c017ff7176d815069bb12de5a4fb4a
Minseo Cho <min1130@ewhain.net>
2023-06-02 14:16:33 +0900

    feat: WebConfig 추가

--- src/main/java/efub/toy/twitter/global/config/WebConfig.java
+++ src/main/java/efub/toy/twitter/global/config/WebConfig.java
@@ -7,19 +7,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:8080")
-                .allowedOrigins("")
-                .allowedOrigins("")
+                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("*")
                 .allowedMethods(HttpMethod.GET.name())
                 .allowedMethods(HttpMethod.POST.name())
                 .allowedMethods(HttpMethod.PUT.name())
                 .allowedMethods(HttpMethod.DELETE.name())
-                .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
                 .allowedHeaders("Authorization")
                 .allowedHeaders("refresh-token")
                 .allowCredentials(true);
