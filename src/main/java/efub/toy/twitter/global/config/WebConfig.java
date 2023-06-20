e53948d985ba31db670eb89a3c55ddf6e82ec5d5
Minseo Cho <min1130@ewhain.net>
2023-06-20 13:25:03 +0900

    feat: WebConfig 추가

--- src/main/java/efub/toy/twitter/global/config/WebConfig.java
+++ src/main/java/efub/toy/twitter/global/config/WebConfig.java
@@ -12,11 +12,14 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("")
+                .allowedOrigins("")
                 .allowedMethods(HttpMethod.GET.name())
                 .allowedMethods(HttpMethod.POST.name())
                 .allowedMethods(HttpMethod.PUT.name())
                 .allowedMethods(HttpMethod.DELETE.name())
-                .allowedHeaders("*")
+                .allowedHeaders("Authorization")
+                .allowedHeaders("refresh-token")
                 .allowCredentials(true);
     }
 }
